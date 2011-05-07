5ccd0bd84fe4ce1ae124f2ae74f54991bedfb8d1
kojitaro <kohura12345678@gmail.com>
2011-05-08 04:27:49 +0900

    adjust a appearance.

--- src/net/hekatoncheir/speechtweet/SpeechTweet.java
+++ src/net/hekatoncheir/speechtweet/SpeechTweet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011 Kouji Ohura
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.hekatoncheir.speechtweet;
 
 import java.util.ArrayList;
@@ -27,7 +42,7 @@ import android.speech.SpeechRecognizer;
 import android.util.Log;
 
 public class SpeechTweet extends Activity {
-	public final static String TAG = "net.hekatoncheir.speechrecognizer";
+	public final static String TAG = "net.hekatoncheir.speechtweet";
 	
 	private final static int ACTIVITY_RESULT_OAUTH = 0;
 
